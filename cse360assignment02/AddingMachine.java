f9c0ab738f684087894aac1a89da4106834a408c
clevinton <clevinton99@gmail.com>
2020-10-05 17:56:14 -0700

    first commit

--- cse360assignment02/AddingMachine.java
+++ cse360assignment02/AddingMachine.java
@@ -2,32 +2,25 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  private String string;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
-    string = "0";
   }
   
   public int getTotal () {
-    return total;
+    return 0;
   }
   
   public void add (int value) {
-    total = total + value;
-    string = string + " + " + value;
   }
 
   public void subtract (int value) {
-    total = total - value;
-    string = string + " - " + value;
   }
 
   public String toString () {
-    return string;
+    return "";
   }
 
   public void clear() {
   }
-  
 }
